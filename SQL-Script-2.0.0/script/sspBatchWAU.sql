d8358b075e1e7f3433445afabc6199d5e3fb639a
JungHyun Kim <junghyun153@naver.com>
2016-04-13 20:42:26 +0900

    SQL: Test the simple query

--- SQL-Script-2.0.0/script/sspBatchWAU.sql
+++ SQL-Script-2.0.0/script/sspBatchWAU.sql
@@ -16,3 +16,28 @@ set @LastRunDT = (dateadd(day, -7, @CurrentDT))
 set @WAU = (select count(*) from members where LastLoginDT between @LastRunDT and @CurrentDT)
 insert into StatsData(CategoryName, CountNum, Fields, Groups) values('WAU', @WAU, CONVERT(nvarchar(8), GETUTCDATE(), 112), '') 
 GO
+
+------------------------------------------------------------------
+-- run test
+--exec sspBatchWAU
+------------------------------------------------------------------
+
+/*
+select * from StatsData order by createdat desc
+select * from Members
+select count(*) from members where LastLoginDT between '2016-05-09 15:00:03.1749825 +00:00' and sysutcdatetime()
+
+-- test data value
+update Members set LastLoginDT = sysutcdatetime() where memberid like 'bbb'
+update Members set LastLoginDT = sysutcdatetime() where memberid like 'ccc'
+update Members set LastLoginDT = sysutcdatetime() where memberid like 'ddd'
+
+select sysutcdatetime()
+select dateadd(day, -7, sysutcdatetime()) 
+select CONVERT(nvarchar(20), getutcdate(), 112)
+
+declare @nowdt datetime
+set @nowdt = (select getutcdate())
+SELECT DATEPART(year, @nowdt) + '-' + DATEPART(month,@nowdt) + '-' +  DATEPART(day, @nowdt);
+SELECT convert(datetime, getutcdate(), 121) -- yyyy-mm-dd hh:mm:ss.mmm 
+*/
