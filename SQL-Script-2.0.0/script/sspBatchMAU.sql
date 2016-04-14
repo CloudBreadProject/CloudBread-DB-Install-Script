c5834ca53fab5c1711d5616bb93cbe4a67897c4a
JungHyun Kim <junghyun153@naver.com>
2016-04-14 20:57:01 +0900

    SPL: Test query for monthly active users

--- SQL-Script-2.0.0/script/sspBatchMAU.sql
+++ SQL-Script-2.0.0/script/sspBatchMAU.sql
@@ -42,4 +42,20 @@ declare @nowdt datetime
 set @nowdt = (select getutcdate())
 SELECT DATEPART(year, @nowdt) + '-' + DATEPART(month,@nowdt) + '-' +  DATEPART(day, @nowdt);
 SELECT convert(datetime, getutcdate(), 121) -- yyyy-mm-dd hh:mm:ss.mmm 
+
+--test month
+declare @nowdt datetime
+declare @CurrentDT datetimeoffset(7)
+declare @testdt datetimeoffset(7)
+declare @testdt2 datetimeoffset(7)
+
+set @nowdt = (select getutcdate())
+set @CurrentDT = ((SELECT DATETIMEFROMPARTS (DATEPART(year, @nowdt), DATEPART(month,@nowdt), DATEPART(day, @nowdt), DATEPART(hour, @nowdt), 0, 0,0 )))
+set @testdt = (dateadd(day, -30, @CurrentDT))
+set @testdt2 = (dateadd(day, -31, @CurrentDT))
+
+update Members set LastLoginDT = @testdt where memberid like 'aaa3'
+update Members set LastLoginDT = @testdt2 where memberid like 'aaa4'
+exec sspBatchMAU
+select * from StatsData where CategoryName like 'MAU' order by createdat desc
 */
