edd9ea43e2662e7b0c9e9eb742fbf4afc1299974
JungHyun Kim <junghyun153@naver.com>
2016-04-15 21:01:24 +0900

    Batch: Added query logic in batch file

--- SQL-Script-2.0.0/install-with-sample.bat
+++ SQL-Script-2.0.0/install-with-sample.bat
@@ -5,6 +5,8 @@ echo off
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBatchDARPU.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBatchDAU.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBatchHAU.sql
+SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBarchMAU.sql
+SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBarchWAU.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/uspAddMemberItemPurchase.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/uspAddUseMemberItem.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/uspComInsMemberItemPurchase.sql
