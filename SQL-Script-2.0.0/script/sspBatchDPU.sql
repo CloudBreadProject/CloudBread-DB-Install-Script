a009b518ac0455bd746829a19e84db87ba6377e3
JungHyun Kim <junghyun153@naver.com>
2016-04-19 21:58:35 +0900

    SQL: Test the simple query

--- SQL-Script-2.0.0/script/sspBatchDPU.sql
+++ SQL-Script-2.0.0/script/sspBatchDPU.sql
@@ -14,3 +14,29 @@ set @CurrentDT = ((SELECT DATETIMEFROMPARTS (DATEPART(year, @nowdt), DATEPART(mo
 set @DPU = (select count(*) from MemberItemPurchases where PurchaseDT between @TodayDT and @CurrentDT)
 insert into StatsData(CategoryName, CountNum, Fields, Groups) values('DPU', @DPU, CONVERT(nvarchar(8), GETUTCDATE(), 112), '') 
 GO
+
+------------------------------------------------------------------
+-- run test
+--exec sspBatchDPU
+------------------------------------------------------------------
+
+/*
+select * from StatsData order by createdat desc
+select * from Members
+select * from MemberItemPurchases
+select count(*) from MemberItemPurchases where PurchaseDT between '2016-05-15 15:00:03.1749825 +00:00' and sysutcdatetime()
+
+-- test data value
+update Members set LastLoginDT = sysutcdatetime() where memberid like 'bbb'
+update Members set LastLoginDT = sysutcdatetime() where memberid like 'ccc'
+update Members set LastLoginDT = sysutcdatetime() where memberid like 'ddd'
+
+select sysutcdatetime()
+select dateadd(day, -1, sysutcdatetime()) 
+select CONVERT(nvarchar(20), getutcdate(), 112)
+
+declare @nowdt datetime
+set @nowdt = (select getutcdate())
+SELECT DATEPART(year, @nowdt) + '-' + DATEPART(month,@nowdt) + '-' +  DATEPART(day, @nowdt);
+SELECT convert(datetime, getutcdate(), 121) -- yyyy-mm-dd hh:mm:ss.mmm 
+*/
