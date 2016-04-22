2367650a11a1ce86924f24d355bc203ce8b220ce
JungHyun Kim <junghyun153@naver.com>
2016-04-22 22:30:01 +0900

    Batch: Added FPU query in batch

--- SQL-Script-2.0.0/install-with-sample.bat
+++ SQL-Script-2.0.0/install-with-sample.bat
@@ -6,6 +6,7 @@ SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBatchDARPU.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBatchDAU.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBatchDormant.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBatchDPU.sql
+SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBatchFPU.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBatchHAU.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBarchMAU.sql
 SQLCMD -S %1 -U %2 -P %3 -d %4 -i ./script/sspBarchWAU.sql
