56cad6b27a4e7d3d9df042f3e37c0f84082b2424
JungHyun Kim <junghyun153@naver.com>
2016-05-02 16:46:03 +0900

    SQL: Basic query test for WPU

--- SQL-Script-2.0.0/script/sspBatchWPU.sql
+++ SQL-Script-2.0.0/script/sspBatchWPU.sql
@@ -14,3 +14,31 @@ set @Day7DT = (dateadd(day, -7, @CurrentDT))
 set @WPU = (select count(*) from MemberItemPurchases where PurchaseDT between @Day7DT and @CurrentDT AND PurchaseCancelYN like 'N')
 insert into StatsData(CategoryName, CountNum, Fields, Groups) values('WPU', @WPU, CONVERT(nvarchar(8), GETUTCDATE(), 112), '') 
 GO
+
+
+------------------------------------------------------------------
+-- run test
+--exec sspBatchWPU
+------------------------------------------------------------------
+
+/*
+select * from StatsData order by createdat desc
+select * from Members
+select * from MemberItemPurchases
+select count(*) from MemberItemPurchases where PurchaseDT between '2016-05-15 15:00:03.1749825 +00:00' and sysutcdatetime()
+
+-- test data value
+update Members set LastLoginDT = sysutcdatetime() where memberid like 'bbb'
+update Members set LastLoginDT = sysutcdatetime() where memberid like 'ccc'
+update Members set LastLoginDT = sysutcdatetime() where memberid like 'ddd'
+
+select sysutcdatetime()
+select dateadd(day, -7, sysutcdatetime()) 
+select CONVERT(nvarchar(20), getutcdate(), 112)
+
+declare @nowdt datetime
+set @nowdt = (select getutcdate())
+SELECT DATEPART(year, @nowdt) + '-' + DATEPART(month,@nowdt) + '-' +  DATEPART(day, @nowdt);
+SELECT convert(datetime, getutcdate(), 121) -- yyyy-mm-dd hh:mm:ss.mmm 
+
+*/
