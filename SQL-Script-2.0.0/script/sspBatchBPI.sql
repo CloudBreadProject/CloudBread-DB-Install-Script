4bd2b6e9a62a3f85d4e98b6c8c8d022130505c71
JungHyun Kim <junghyun153@naver.com>
2016-05-30 19:09:39 +0900

    SQL: Fixed BPI query because of adding the BPI number logic

--- SQL-Script-2.0.0/script/sspBatchBPI.sql
+++ SQL-Script-2.0.0/script/sspBatchBPI.sql
@@ -9,6 +9,10 @@ declare @BPI1 nvarchar(12)
 declare @BPI2 nvarchar(12)
 declare @BPI3 nvarchar(12)
 
+declare @BPI1num bigint
+declare @BPI2num bigint
+declare @BPI3num bigint
+
 set @nowdt = (select getutcdate())
 --select @nowdt as nowdt
 set @CurrentDT = ((SELECT DATETIMEFROMPARTS (DATEPART(year, @nowdt), DATEPART(month, @nowdt), DATEPART(day, @nowdt), DATEPART(hour, @nowdt), 0, 0, 0)))
@@ -16,21 +20,21 @@ set @CurrentDT = ((SELECT DATETIMEFROMPARTS (DATEPART(year, @nowdt), DATEPART(mo
 set @Last30DT = (dateadd(day, -30, @CurrentDT))
 --select @Last30DT as Last30DT
 
-set @BPI1 = (select ItemListID from (select ItemListID, row_number() over(order by t1.TotalCount desc) as rownum from (select TOP 3 ItemListID, count(*) AS TotalCount from MemberItemPurchases where PurchaseDT between @Last30DT and @CurrentDT group by ItemListID order by TotalCount desc) t1) t1 where t1.rownum like '1')
-set @BPI2 = (select ItemListID from (select ItemListID, row_number() over(order by t1.TotalCount desc) as rownum from (select TOP 3 ItemListID, count(*) AS TotalCount from MemberItemPurchases where PurchaseDT between @Last30DT and @CurrentDT group by ItemListID order by TotalCount desc) t1) t1 where t1.rownum like '2')
-set @BPI3 = (select ItemListID from (select ItemListID, row_number() over(order by t1.TotalCount desc) as rownum from (select TOP 3 ItemListID, count(*) AS TotalCount from MemberItemPurchases where PurchaseDT between @Last30DT and @CurrentDT group by ItemListID order by TotalCount desc) t1) t1 where t1.rownum like '3')
+(select @BPI1 = ItemListID, @BPI1num = TotalCount from (select ItemListID, row_number() over(order by t1.TotalCount desc) as rownum, TotalCount from (select TOP 3 ItemListID, count(*) AS TotalCount from MemberItemPurchases where PurchaseDT between @Last30DT and @CurrentDT group by ItemListID order by TotalCount desc) t1) t2 where t2.rownum like '1')
+(select @BPI2 = ItemListID, @BPI2num = TotalCount from (select ItemListID, row_number() over(order by t1.TotalCount desc) as rownum, TotalCount from (select TOP 3 ItemListID, count(*) AS TotalCount from MemberItemPurchases where PurchaseDT between @Last30DT and @CurrentDT group by ItemListID order by TotalCount desc) t1) t2 where t2.rownum like '2')
+(select @BPI3 = ItemListID, @BPI3num = TotalCount from (select ItemListID, row_number() over(order by t1.TotalCount desc) as rownum, TotalCount from (select TOP 3 ItemListID, count(*) AS TotalCount from MemberItemPurchases where PurchaseDT between @Last30DT and @CurrentDT group by ItemListID order by TotalCount desc) t1) t2 where t2.rownum like '3')
 
 if @BPI1 is not null
 	begin
-		insert into StatsData(CategoryName, CountNum, Fields, Groups) values('BPItem1', 0, CONVERT(nvarchar(8), GETUTCDATE(), 112), @BPI1)
+		insert into StatsData(CategoryName, CountNum, Fields, Groups) values('BPItem1', @BPI1num, CONVERT(nvarchar(8), GETUTCDATE(), 112), @BPI1)
 	end
 if @BPI2 is not null
 	begin
-		insert into StatsData(CategoryName, CountNum, Fields, Groups) values('BPItem2', 0, CONVERT(nvarchar(8), GETUTCDATE(), 112), @BPI2)
+		insert into StatsData(CategoryName, CountNum, Fields, Groups) values('BPItem2', @BPI2num, CONVERT(nvarchar(8), GETUTCDATE(), 112), @BPI2)
 	end
 if @BPI3 is not null
 	begin
-		insert into StatsData(CategoryName, CountNum, Fields, Groups) values('BPItem3', 0, CONVERT(nvarchar(8), GETUTCDATE(), 112), @BPI3)
+		insert into StatsData(CategoryName, CountNum, Fields, Groups) values('BPItem3', @BPI3num, CONVERT(nvarchar(8), GETUTCDATE(), 112), @BPI3)
 	end
 GO
 
